0a610f23b7aaa0b3bb52eb89e191b416dc9b0f55
Amanda-Cole <col22031@byui.edu>
2023-01-11 13:44:07 -0700

    Week 2 prj5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
+    // function Main
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Initialize function DisplayWelcomeMessage
+        DisplayWelcomeMessage();
+
+        // set variable return for function PromptUserName and PromptUserNumber
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        // set variable return for function SquareNumber using the userNumber
+        int squaredNumber = SquareNumber(userNumber);
+
+        // Initialize function DisplayResult with parameters of userName and userNumber
+        DisplayResult(userName,squaredNumber);
     }
+// Defining the functions that are called in Main
+    static  void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your Name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+   
 }
