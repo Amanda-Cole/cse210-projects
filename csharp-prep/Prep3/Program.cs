1b692c6787f873647df95faa9ae24d3b09f95cae
Amanda-Cole <col22031@byui.edu>
2023-01-11 11:41:52 -0700

    Week 2 prj 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number between 1-10? ");
+        // string number = Console.ReadLine();
+        // int magicNumber = int.Parse(number);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guessNumber = -1;
+        // string playon = "yes";
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess between 1-100? ");
+            guessNumber =int.Parse(Console.ReadLine());
+            
+
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
